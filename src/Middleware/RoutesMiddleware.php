e91ff905fd80a2a98585178681f8b7ce986b637b
candra rahmawan <candraa.rahmawan@gmail.com>
2019-04-19 11:24:37 +0700

    Fix middleware routes for file

--- src/Middleware/RoutesMiddleware.php
+++ src/Middleware/RoutesMiddleware.php
@@ -7,7 +7,14 @@ class RoutesMiddleware
     public function forceTrailingSlash()
     {
         $requestUri = $_SERVER['REQUEST_URI'];
-        if (substr($requestUri, -1) != '/') {
+        $exceptCharacter = ['.css', '.js', '.png', '.jpg', '.jpeg', '.json'];
+        $except = false;
+        foreach ($exceptCharacter as $item) {
+            if (strpos($requestUri, $item) !== false) {
+                $except = true;
+            }
+        }
+        if (substr($requestUri, -1) != '/' && $except == false) {
             header("Location: " . $requestUri . "/");
             return true;
         }
