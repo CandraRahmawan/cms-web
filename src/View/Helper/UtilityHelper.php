263b58781238a26380794b651562135f33ac12cb
Candra Rahmawan <candraa.rahmawan@gmail.com>
2020-04-09 21:01:22 +0700

    CR: read https by env

--- src/View/Helper/UtilityHelper.php
+++ src/View/Helper/UtilityHelper.php
@@ -21,10 +21,8 @@ class UtilityHelper extends Helper {
   
   public function buildFullUrl($url) {
     $queryBuilder = sizeof($this->request->query) > 0 ? '?' . http_build_query($this->request->query) : '';
-    $http = 'https://';
-    if ($this->isDevelopment()) {
-      $http = '//';
-    } else {
+    $http = $this->request->env('HTTPS') ? 'https://' : '//';
+    if (!$this->isDevelopment()) {
       $url = str_replace($this->request->base, "", $url);
     }
     return $http . $this->request->env('HTTP_HOST') . $url . $queryBuilder;
