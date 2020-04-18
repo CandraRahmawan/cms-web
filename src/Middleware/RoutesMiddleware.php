30c1f6e9eed36c2101a5e8161137a9a158d2afb2
Candra Rahmawan <candraa.rahmawan@gmail.com>
2020-04-18 12:31:56 +0700

    CR: Fix force redirect www when env WITH_WWW is true

--- src/Middleware/RoutesMiddleware.php
+++ src/Middleware/RoutesMiddleware.php
@@ -11,6 +11,7 @@ class RoutesMiddleware {
   
   public function __invoke($request, $response, $next) {
     $requestUri = $_SERVER['REQUEST_URI'];
+    $serverName = $_SERVER['SERVER_NAME'];
     $exceptCharacter = ['.css', '.js', '.png', '.jpg', '.jpeg', '.json', '.svg', '.txt', '.xml'];
     $except = false;
     foreach ($exceptCharacter as $item) {
@@ -18,10 +19,11 @@ class RoutesMiddleware {
         $except = true;
       }
     }
-    if (substr($requestUri, -1) != '/' && $except == false) {
+    if ((substr($requestUri, -1) != '/' && $except == false) || ($except == false && strstr($serverName, 'www'))) {
       return $response->withStatus(301)
         ->withHeader('Location', Configure::read('App.baseWebUrl') . $requestUri . "/");
     }
+    
     return $next($request, $response);
   }
 }
